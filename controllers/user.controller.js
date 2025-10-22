2419d202c80acb45ea1651eb1c043dea39320602
thiagosullivan <thiagosullivan12@gmail.com>
2025-10-22 14:22:50 -0300

    feat: add login and logout routes and logic, add auth middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,8 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import sendEmailFun from "../config/sendEmail.js";
 import VerificationEmailTemplate from "../utils/verifyEmailTemplate.js";
-import { error } from "console";
+import generatedAccessToken from "../utils/generatedAccessToken.js";
+import generatedRefreshToken from "../utils/generatedRefreshToken.js";
 
 export async function registerUserController(request, response) {
   try {
@@ -115,6 +116,104 @@ export async function verifyEmailController(request, response) {
         message: "OTP expired",
       });
     }
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function loginUserController(request, response) {
+  try {
+    const { email, password } = request.body;
+
+    const user = await UserModel.findOne({ email: email });
+    console.log(user, "USER");
+
+    if (!user) {
+      response.status(400).json({
+        message: "User not register",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "Active") {
+      response.status(400).json({
+        message: "Contact to admin",
+        error: true,
+        success: false,
+      });
+    }
+
+    const checkPassword = await bcryptjs.compare(password, user.password);
+
+    if (!checkPassword) {
+      response.status(400).json({
+        message: "Check your password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accessToken = await generatedAccessToken(user._id);
+    const refreshToken = await generatedRefreshToken(user._id);
+
+    const updateUser = await UserModel.findByIdAndUpdate(user?._id, {
+      last_login_date: new Date(),
+    });
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    response.cookie("accessToken", accessToken, cookiesOption);
+    response.cookie("refreshToken", refreshToken, cookiesOption);
+
+    return response.json({
+      message: "Login successfully",
+      error: false,
+      success: true,
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function logoutController(request, response) {
+  try {
+    const userId = request.userId;
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    response.clearCookie("accessToken", cookiesOption);
+    response.clearCookie("refreshToken", cookiesOption);
+
+    const removeRefreshToken = await UserModel.findByIdAndUpdate(userId, {
+      refresh_token: "",
+    });
+
+    return response.json({
+      message: "Logout successfully",
+      error: false,
+      success: true,
+    });
   } catch (error) {
     return response.status(500).json({
       message: error.message || error,
--- models/user.model.js
+++ models/user.model.js
@@ -31,6 +31,14 @@ const userSchema = mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    access_token: {
+      type: String,
+      default: "",
+    },
+    refresh_token: {
+      type: String,
+      default: "",
+    },
     last_login_date: {
       type: Date,
       default: "",
